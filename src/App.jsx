36d801e8af3e624b2d9cf38aaa520fd9ac316450
Behnaz <behnaz.shojaei88@gmail.com>
2024-11-16 08:03:23 +0800

    setMessage and update button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from 'react'
-// useState is a special function from react
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
 function App() {
-  
-  const [message,setMessage] = useState("Hi, all!")
+  const [message, setMessage] = React.useState("JavaScript is so cool.");
 
-  const updateMessage = () =>{
-    setMessage("Hello, React!")
-
+  function handleClick() {
+    setMessage("I love React");
   }
-
   return (
   <div>
     <h1>{message}</h1>
-    <button onclick={updateMessage}>Update Message</button>
-    
+    <button onClick={handleClick}>Update the message!</button>
     </div>
-  
-  );
+    );
 }
 
-export default App
+export default App;
